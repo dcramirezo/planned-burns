ad33bf867766d4aa62048740a4e05c50040726c7
kasracodeforaustralia <ka10sra@yahoo.com>
2016-08-24 10:11:12 +1000

    Add files via upload

---
+++ classes/connect_to_db.php
@@ -0,0 +1,10 @@
+<?php
+       // connect to database
+	$hostName = "adkaselcom.fatcowmysql.com";
+	$dbName = "planned_burns1";
+	$username= "delwp";
+	$password = "delwp";
+			
+	mysql_connect($hostName, $username, $password) or die ("</br>Cannot connect to database! </br>");
+
+?>
